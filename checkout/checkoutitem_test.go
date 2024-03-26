d282ddb7b7bad048a52235a43c8435b51c9c6e99
Billie M <billieeemx@gmail.com>
2024-03-26 23:21:12 +0000

    checkout item getPrice test

--- checkout/checkoutitem_test.go
+++ checkout/checkoutitem_test.go
@@ -1,1 +1,58 @@
 package checkout
+
+import "testing"
+
+func TestCheckoutItem_GetPrice(t *testing.T) {
+	tests := []struct {
+		name          string
+		unitPrice     int
+		offerQuantity int
+		offerPrice    int
+		quantity      int
+		expected      int
+	}{
+		{
+			name:          "No offer",
+			unitPrice:     10,
+			offerQuantity: 0,
+			offerPrice:    0,
+			quantity:      5,
+			expected:      50,
+		},
+		{
+			name:          "With offer, quantity less than offer quantity",
+			unitPrice:     7,
+			offerQuantity: 6,
+			offerPrice:    25,
+			quantity:      5,
+			expected:      35,
+		},
+		{
+			name:          "With offer, quantity equal to offer quantity",
+			unitPrice:     1010,
+			offerQuantity: 6,
+			offerPrice:    25,
+			quantity:      6,
+			expected:      25,
+		},
+		{
+			name:          "With offer, quantity greater than offer quantity",
+			unitPrice:     15,
+			offerQuantity: 6,
+			offerPrice:    25,
+			quantity:      7,
+			expected:      40,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			item := checkoutItem{}
+
+			actualPrice := item.getPrice()
+			if actualPrice != tt.expected {
+				t.Errorf("Expected price: %d, got: %d", tt.expected, actualPrice)
+			}
+		})
+	}
+}
